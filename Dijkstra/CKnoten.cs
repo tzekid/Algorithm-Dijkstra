c119267dc6423f430cfd8054ca5431e95743e6e7
lukasroehr <lukasroehr@hotmail.de>
2014-12-01 20:37:20 +0100

    Version 0.1

---
+++ Dijkstra/CKnoten.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    class CKnoten
+    {
+        string name;
+        string state;
+        public CKnoten(string name, string state)
+        {
+            this.name = name;
+            this.state = state;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetState()
+        {
+            return state;
+        }
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+    }
+}
